cfa67df5117606f7c21c9236af5ffd4f94390048
Alan Cano <alancorreonuevo5@gmail.com>
2023-11-08 15:14:19 -0600

    Discord code works, but discord bot is not appearing online

--- main.go
+++ main.go
@@ -1,47 +1,44 @@
 package main
 
 import (
-	"flag"
-	"fmt"
+	"log"
+	"os"
 
-	"github.com/Stransyyy/Sheet-Linker/db"
+	"github.com/Stransyyy/Sheet-Linker/dc"
 )
 
 func main() {
-	cred, err := db.JsonFileReader("credentials.json")
-	if err != nil {
-		return
+	// cred, err := db.JsonFileReader("credentials.json")
+	// if err != nil {
+	// 	return
+	// }
+
+	// fmt.Println("Welcome to MySQL")
+
+	// con, err := db.Connection(cred)
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// db.ScanTableInputs(con)
+
+	// fmt.Println("inputs added ")
+
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	botToken, ok := os.LookupEnv("DISCORD_APIKEY")
+	if !ok {
+		log.Fatal("Must set Discord token as env variable: DISCORD_APIKEY")
 	}
 
-	fmt.Println("Welcome to MySQL")
+	dc.BotToken = botToken
+	dc.Run()
 
-	con, err := db.Connection(cred)
-	if err != nil {
-		panic(err)
-	}
-
-	db.ScanTableInputs(con)
-
-	fmt.Println("inputs added ")
-
-	// Discord Stuff
-	var Token string
-
-	flag.StringVar(&Token, "t", "", "Bot token")
-	flag.Parse()
-
-	if Token == "" {
-		fmt.Println("Please provide a bot using the -t flag.")
-		return
-	}
-
-	hola := Runbot("fdf442234")
-
-	fmt.Print(hola)
+	//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 	// Closes connection to the databse
-	err = db.CloseDB(con)
-	if err != nil {
-		panic(err)
-	}
+	// err = db.CloseDB(con)
+	// if err != nil {
+	// 	panic(err)
+	// }
 }
